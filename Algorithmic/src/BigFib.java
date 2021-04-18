1fcbfc1c0565787fabb3a71ae5a524e87c95a3e4
Veronika Kitsul <65815639+Veronika-Kitsul@users.noreply.github.com>
2021-04-17 23:22:53 -0400

    /

--- Algorithmic/src/BigFib.java
+++ Algorithmic/src/BigFib.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 public class BigFib {
 
 	
+	public BigFib(BigInteger number) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public BigInteger BigFib(BigInteger n) 
 	{
 		BigInteger fib = new BigInteger("1");
+		BigInteger mult;
 		if (n.equals(1) || n.equals(2))
 		{
 			System.out.println(fib);
@@ -14,17 +19,19 @@ public class BigFib {
 		}
 		else 
 		{
-			BigInteger mult = new BigInteger(BigFib(new BigInteger("n/2 - 1")).multiply(new BigInteger("2")));
-			BigInteger two = new BigInteger(BigFib(new BigInteger("n/2")).add(mult));
-			fib = two.multiply((BigFib(new BigInteger("n/2")).add(mult)));
+			BigInteger mult1 = (BigFib(new BigInteger("n/2 - 1"))).multiply(new BigInteger("2"));
+			BigInteger two = BigFib(new BigInteger("n/2")).add(mult1);
+			fib = two.multiply((BigFib(new BigInteger("n/2")).add(mult1)));
 		}
+		return fib;
 	}
-	
+
 	public static void main(String[] args)
 	{
 		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		BigInteger number = new BigInteger("n");
-		new BigFib(number);
+		BigInteger number = BigInteger.valueOf(scan.nextInt());
+		BigFib result = new BigFib(number);
+		System.out.println(result);
+		scan.close();
 	}
 }
